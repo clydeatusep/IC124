615506feeb48dff27f45fe92ad496978052675db
CCRB <159453224+clydeatusep@users.noreply.github.com>
2024-02-22 17:36:20 +0800

    Create TemperatureDemo.java

---
+++ M2/Temperature/TemperatureDemo.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class TemperatureDemo{
+   public static void main(String [] args){
+      // Create new object
+      Temperature tmpobj1 = new Temperature();
+      
+      tmpobj1.displayTemp(1, 4);
+      tmpobj1.displayAveTemp(1);
+   }
+}
