e82aba7918c7fa65faf438ed31162a12fe169747
CCRB <159453224+clydeatusep@users.noreply.github.com>
2024-04-22 00:14:42 +0800

    Update ChangingFace.java

--- M5/ChangingFace.java
+++ M5/ChangingFace.java
@@ -13,61 +13,83 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.geometry.Pos;
-public class ChangingFace extends Application {
+
+public class ChangingFace extends Application
+{
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage)
+    {
         // create and configure the main circle for the face
-        Circle face = new Circle(125, 125, 80);
+        Circle face = new Circle(125, 125, 80); 
         face.setFill(Color.YELLOW);
         face.setStroke(Color.RED);
-        // create and configure the circle for the
+        
+        // create and configure the circle for the right eye
         Circle rightEye = new Circle(86, 100, 10);
         rightEye.setFill(Color.YELLOW);
         rightEye.setStroke(Color.BLUE);
-        // create and configure the circle for the
+        
+        // create and configure the circle for the left eye
         Circle leftEye = new Circle(162, 100, 10);
         leftEye.setFill(Color.YELLOW);
         leftEye.setStroke(Color.BLUE);
+        
         // create and configure a smiling mouth (this is how it will start)
-        Arc mouth = new Arc(125, 150, 45, 35, 0, -180);
+        Arc mouth = new Arc(125, 150, 45, 35, 0, -180); 
         mouth.setFill(Color.YELLOW);
         mouth.setStroke(Color.BLUE);
         mouth.setType(ArcType.OPEN);
+        
         // create and configure the text
         Text caption = new Text(68, 240, "Changing Face");
         caption.setFill(Color.BLUE);
-        caption.setFont(Font.font("Verdana", 15));
-        // create a group that holds all the features
-        Group group = new Group(face, rightEye, leftEye, mouth, caption);
+        caption.setFont(Font.font ("Verdana", 15));
+                
+        // create a group that holds all the features  
+        Group group = new Group(face, rightEye, leftEye, mouth,  caption);
+
         // create a button that will make the face smile
         Button smileButton = new Button("Smile");
+                            
         // create a button that will make the face frown
         Button frownButton = new Button("Frown");
+                   
+     
         // create and configure a horizontal container to hold the buttons
         HBox buttonBox = new HBox(10);
         buttonBox.setAlignment(Pos.CENTER);
+        
         //add the buttons to the horizontal container
         buttonBox.getChildren().addAll(smileButton, frownButton);
+        
         // create and configure a vertical container to hold the button box and the face group
         VBox root = new VBox(10);
-
         root.setBackground(Background.EMPTY);
         root.setAlignment(Pos.CENTER);
+        
         //add the button box and the face group to the vertical container
         root.getChildren().addAll(buttonBox, group);
+        
         // create and configure a new scene
         Scene scene = new Scene(root, 250, 275, Color.YELLOW);
+
         // supply the code that is executed when the smile button is pressed
-        smileButton.setOnAction(e - > mouth.setLength(-180));
+        smileButton.setOnAction(e -> mouth.setLength(-180));
+        
         // supply the code that is executed when the frown button is pressed
-        frownButton.setOnAction(e - > mouth.setLength(180));
+        frownButton.setOnAction(e -> mouth.setLength(180));
+         
         // add the scene to the stage, then set the title
         stage.setScene(scene);
         stage.setTitle("Changing Face");
+
         // show the stage
-        stage.show();
-    }
-    public static void main(String[] args) {
+        stage.show();        
+    }    
+    
+    public static void main(String[] args)
+    {
         launch(args);
     }
+    
 }
