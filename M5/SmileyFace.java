fba591f51c1b009be7ef98c4651520575331c62f
CCRB <159453224+clydeatusep@users.noreply.github.com>
2024-04-22 00:14:01 +0800

    Update SmileyFace.java

--- M5/SmileyFace.java
+++ M5/SmileyFace.java
@@ -8,41 +8,57 @@ import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-public class SmileyFace extends Application {
+
+
+public class SmileyFace extends Application
+{
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage)
+    {
         // create and configure the main circle for the face
-        Circle face = new Circle(125, 125, 80);
+        Circle face = new Circle(125, 125, 80); 
         face.setFill(Color.YELLOW);
         face.setStroke(Color.RED);
+        
         // create and configure the circle for the right eye
         Circle rightEye = new Circle(86, 100, 10);
         rightEye.setFill(Color.YELLOW);
         rightEye.setStroke(Color.BLUE);
+        
         // create and configure the circle for the left eye
         Circle leftEye = new Circle(162, 100, 10);
         leftEye.setFill(Color.YELLOW);
         leftEye.setStroke(Color.BLUE);
+        
         // create and configure a smiling mouth
-        Arc mouth = new Arc(125, 150, 45, 35, 0, -180);
+        Arc mouth = new Arc(125, 150, 45, 35, 0, -180); 
         mouth.setFill(Color.YELLOW);
         mouth.setStroke(Color.BLUE);
         mouth.setType(ArcType.OPEN);
+        
         // create and configure the text
         Text caption = new Text(80, 240, "Smiley Face");
         caption.setFill(Color.BLUE);
         caption.setFont(Font.font("Verdana", 15));
-        // create a group that holds all the features
+        
+      
+        // create a group that holds all the features            
         Group root = new Group(face, rightEye, leftEye, mouth, caption);
+ 
         // create and configure a new scene
         Scene scene = new Scene(root, 250, 275, Color.YELLOW);
+
         // add the scene to the stage, then set the title
         stage.setScene(scene);
         stage.setTitle("Smiley Face");
+
         // show the stage
-        stage.show();
+        stage.show();        
     }
-    public static void main(String[] args) {
+    
+    public static void main(String[] args)
+    {
         launch(args);
     }
+    
 }
