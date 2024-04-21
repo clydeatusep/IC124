2332621b60868815475be3b669a91cea90978fd6
CCRB <159453224+clydeatusep@users.noreply.github.com>
2024-04-21 21:28:42 +0800

    Update BankAccount.java

--- M2/BankAccount/BankAccount.java
+++ M2/BankAccount/BankAccount.java
@@ -1,64 +1,53 @@
-public class BankAccount {
+
+public class BankAccount
+{
+    // the attributes
     private String accountNumber;
     private String accountName;
     private double balance;
 
-    public BankAccount(String accountNumber, String accountName, double balance) {
-        this.accountNumber = accountNumber;
-        this.accountName = accountName;
-        this.balance = balance;
+    // the methods
+
+    // the constructor
+    public BankAccount(String numberIn, String nameIn)
+    {
+        accountNumber = numberIn;
+        accountName = nameIn;
+        balance = 0;
     }
 
-    // Getter methods
-    public String getAccountNumber() {
-        return accountNumber;
-    }
-
-    public String getAccountName() {
+    // methods to read the attributes
+    public String getAccountName()
+    {
         return accountName;
     }
 
-    public double getBalance() {
+    public String getAccountNumber()
+    {
+        return accountNumber;
+    }
+
+    public double getBalance()
+    {
         return balance;
     }
 
-    // Setter methods
-    public void setAccountNumber(String accountNumber) {
-        this.accountNumber = accountNumber;
+    // methods to deposit and withdraw money
+    public void deposit(double amountIn)
+    {
+        balance = balance + amountIn;
     }
-
-    public void setAccountName(String accountName) {
-        this.accountName = accountName;
-    }
-
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
-
-    public void deposit(double amount) {
-        if (amount > 0) {
-            balance += amount;
-            System.out.println("Deposit of $" + amount + " successful. New balance: $" + balance);
-        } else {
-            System.out.println("Invalid deposit amount. Please enter a positive value.");
+    
+    public boolean withdraw(double amountIn)
+    {
+        if(amountIn > balance)
+        {
+            return false; // no withdrawal was made
         }
-    }
-
-    public boolean withdraw(double amount) {
-        if (amount > 0 && amount <= balance) {
-            balance -= amount;
-            System.out.println("Withdrawal of $" + amount + " successful. New balance: $" + balance);
-            return true;
-        } else {
-            System.out.println("Insufficient funds or invalid withdrawal amount.");
-            return false;
+        else
+        {
+            balance = balance - amountIn;
+            return true; // money was withdrawn successfully
         }
-    }
-
-    public static void main(String[] args) {
-        BankAccount account = new BankAccount("123456789", "John Doe", 1000.0);
-        account.deposit(500.0);
-        account.withdraw(200.0);
-        account.withdraw(1500.0);
     }
 }
